185be1058ed4ac7889ccf122b837cef1c567523a
G Phani <G Phani>
2016-05-23 22:11:10 +0530

    Initial commit

---
+++ app/src/main/java/com/mvp/LoginView.java
@@ -0,0 +1,10 @@
+package com.mvp;
+
+/**
+ * Created by Phani on 23-May-16.
+ */
+public interface LoginView {
+    void setUsernameError();
+
+    void setPassWordError();
+}
---
+++ app/src/main/java/com/mvp/MainActivity.java
@@ -0,0 +1,59 @@
+package com.mvp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements LoginView {
+
+    EditText uname, pwd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        uname = (EditText) findViewById(R.id.uname);
+        pwd = (EditText) findViewById(R.id.pwds);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void setUsernameError() {
+        uname.setError("Enter UserName");
+    }
+
+    @Override
+    public void setPassWordError() {
+        pwd.setError("Enter Password");
+    }
+}
